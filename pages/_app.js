198bcbcf5ae2e9d763a04820774a8f6a3b422c5e
julienbrg <julien@strat.cc>
2023-02-13 19:16:15 +0100

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,27 @@
-
+/* eslint-disable prettier/prettier */
 import '../styles/globals.css'
-import { PlasmicRootProvider } from "@plasmicapp/react-web";
-import Head from "next/head";
+import { PlasmicRootProvider } from '@plasmicapp/react-web'
+import Head from 'next/head'
+import { Web3Provider } from 'providers/Web3'
+import { ChakraProvider } from 'providers/Chakra'
+import { useIsMounted } from 'hooks/useIsMounted'
+import { Seo } from 'components/layout/Seo'
 
-function MyApp({ Component, pageProps }) {
+function App({ Component, pageProps }) {
+  const isMounted = useIsMounted()
+
   return (
-    <PlasmicRootProvider Head={Head}>
-      <Component {...pageProps} />
-    </PlasmicRootProvider>
-  );
+    <ChakraProvider>
+      <Seo />
+      <Web3Provider>
+        {isMounted && (
+          <PlasmicRootProvider Head={Head}>
+            <Component {...pageProps} />
+          </PlasmicRootProvider>
+        )}
+      </Web3Provider>
+    </ChakraProvider>
+  )
 }
 
-export default MyApp
-  
+export default App
